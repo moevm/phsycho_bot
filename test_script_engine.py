e9058a4eed3200fe554af4d5ea83f328919e2d9c
Anya18 <unknownladyspb@gmail.com>
2021-11-19 10:35:06 +0300

    Add tests for Script and Parser

---
+++ test_script_engine.py
@@ -0,0 +1,41 @@
+import unittest
+from json.decoder import JSONDecodeError
+from telegram import Update
+import pytz
+import datetime
+
+from db import User, SurveyProgress
+from script_engine import Script, Parser, Step
+
+
+class TestScript(unittest.TestCase):
+
+    def test_get_script(self):
+        self.assertEqual(Script({'f_tired': {'script_body': [{'script_name': 'f_tired', 'options':
+                [{'type': 'send_message', 'text': 'message'}]}, {'script_name': 'f_tired', 'id': 'tired_0.1'}]}}).get_script('f_tired'), [
+                {'script_name': 'f_tired', 'options': [{'type': 'send_message', 'text': 'message'}]}, {'script_name': 'f_tired', 'id': 'tired_0.1'}])
+        self.assertRaises(KeyError, Script({'f_tired': {'script_body': [{'script_name': 'f_tired', 'options':
+                [{'type': 'send_message', 'text': 'message'}]}, {'script_name': 'f_tired', 'id': 'tired_0.1'}]}}).get_script('ff_tired'))
+
+
+class TestParser(unittest.TestCase):
+
+    def test_parse(self):
+        self.assertEqual(Parser("test_tree.json").parse(),
+                         {'f_tired': {'script_body': [{'script_name': 'f_tired', 'options':
+                    [{'type': 'send_message', 'text': 'message'}]}, {'script_name': 'f_tired', 'id': 'tired_0.1'}]}})
+        self.assertRaises(FileNotFoundError, Parser("test_treee.json").parse())
+        self.assertRaises(JSONDecodeError, Parser("test_broken_tree.json").parse())
+
+
+class TestStep(unittest.TestCase):
+
+    def test_processing_options(self):
+        pass
+
+    def test_execute(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
