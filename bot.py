b5e67beff5743ac38db67bd627aac7dfb47c95fb
Anya18 <unknownladyspb@gmail.com>
2021-12-09 21:15:21 +0300

    Add /get_users_not_finish_survey

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Callback
     ConversationHandler, MessageHandler, Filters
 
 from db import push_user_feeling, push_user_focus, push_user_schedule, get_user_feelings, \
-    set_user_ready_flag, set_schedule_asked_today, init_user, get_schedule_by_user, auth_in_db
+    set_user_ready_flag, set_schedule_asked_today, init_user, get_schedule_by_user, auth_in_db, \
+    get_users_not_finish_survey
 from keyboard import daily_schedule_keyboard, mood_keyboard, focus_keyboard, ready_keyboard, \
     VALUES
 from script_engine import Engine
@@ -93,6 +94,10 @@ def error(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Error!')
 
 
+def debug_get_users_not_finish_survey(update: Update, context: CallbackContext):
+    update.message.reply_text('\n'.join(str(item) for item in get_users_not_finish_survey()))
+    
+
 def ask_ready(updater, schedule):
     # set_schedule_is_on_flag(schedule, False)
     set_schedule_asked_today(schedule)
@@ -126,6 +131,8 @@ def main(token):
 
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_handler(CommandHandler('stats', stats))
+
+    updater.dispatcher.add_handler(CommandHandler('get_users_not_finish_survey', debug_get_users_not_finish_survey))
 
     updater.dispatcher.add_handler(ConversationHandler(
         entry_points=[CallbackQueryHandler(button)],
