a46aa3b70a1c119618c023482dbe6e4c003293cd
Yelizaveta <elizavetaa40@gmail.com>
2023-06-20 23:21:10 +0300

    SSML added

--- silero_demo/silero_test.py
+++ silero_demo/silero_test.py
@@ -2,14 +2,23 @@ import torch
 import time
 import sounddevice as sd
 import torchaudio
+import re
 
 language = 'ru'
 model_id = 'v3_1_ru'
 sample_rate = 48000
 speaker = 'aidar'
 device = torch.device('cpu')
-example_text = 'Привет я бот-психолог'
+example_text = "Привет. я бот-психолог"
 
+
+def reformat_text(text):
+    paragraphs = text.split('\n')
+    for i in range(len(paragraphs)):
+        sentences = ["<s>" + elem + "</s>" for elem in re.split(r'(?<=[.?!])\s+', paragraphs[i])]
+        paragraphs[i] = '<p>' + ' '.join(sentences) + '</p>'
+    text = "<speak>" + '\n'.join(paragraphs) + "</speak>"
+    return text
 
 def silero_test(text=example_text):
     model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
@@ -18,7 +27,9 @@ def silero_test(text=example_text):
                                          speaker=model_id)
     model.to(device)  # gpu or cpu
 
-    audio = model.apply_tts(text=text,
+    text = reformat_text(text)
+
+    audio = model.apply_tts(ssml_text=text,
                             speaker=speaker,
                             sample_rate=sample_rate)
     filename = 'test_1.wav'
