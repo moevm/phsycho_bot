ce38896cd0c53ac776f55f2913782b7e6fa55d68
Elizaveta Andreeva <and_reevaa@test.com>
2024-04-11 21:47:15 +0300

    add TOKEN

--- env_config.py
+++ env_config.py
@@ -6,5 +6,6 @@ SAMPLE_RATE = getenv('SAMPLE_RATE')
 SPEAKER = getenv('SPEAKER')
 ASR_ENGINE = getenv('ASR_ENGINE')
 ASR_MODEL = getenv('ASR_MODEL')
+TOKEN = getenv('TELEGRAM_TOKEN')
 
 DEBUG_ON, DEBUG_OFF = "true", "false"
