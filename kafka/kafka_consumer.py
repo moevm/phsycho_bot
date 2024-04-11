5d743d2144452d4fa66ee087238f0c538283295a
Elizaveta Andreeva <and_reevaa@test.com>
2024-04-11 21:48:05 +0300

    Ðadd configuration from json

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -6,20 +6,27 @@ import json
 
 from kafka import kafka_operations
 from voice_module import audio_to_text
+from wrapper import send_voice
+
+conf = kafka_operations.load_conf('kafka/consumer_conf.json')
 
 
 def process_stt_message(message_info):
     message_info = json.loads(message_info)
     user = User(**message_info['user'])
-    audio_to_text(message_info['token'], message_info['filename'], message_info['ogg_filename'],
+    audio_to_text(message_info['filename'], message_info['ogg_filename'],
                   message_info['update_id'], user)
 
 
+def process_tts_message(message_info):
+    message_info = json.loads(message_info)
+    user = User(**message_info['user'])
+    send_voice(message_info['text'], user, message_info['reply_markup'])
+
+
 def main():
-    print(os.getcwd(), os.listdir())
-    conf = kafka_operations.load_conf('kafka/consumer_conf.json')
     consumer = Consumer(conf)
-    consumer.subscribe(['stt'])
+    consumer.subscribe(['stt', 'tts'])
 
     while True:
         message = consumer.poll(0)
@@ -32,4 +39,7 @@ def main():
 
         # print("Connected to Topic: {} and Partition : {}".format(message.topic(), message.partition()))
         # print("Received Message : {} with Offset : {}".format(message.value().decode('utf-8'), message.offset()))
-        process_stt_message(message.value().decode('utf-8'))
+        if message.topic() == 'stt':
+            process_stt_message(message.value().decode('utf-8'))
+        elif message.topic() == 'tts':
+            process_tts_message(message.value().decode('utf-8'))
