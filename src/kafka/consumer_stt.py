61bd9b68cb16042b6a012585294e4d27919ce8cd
elizaveta-andreeva <elizavetaa40@gmail.com>
2024-05-29 21:58:34 +0300

    add stt and tts consumers

---
+++ src/kafka/consumer_stt.py
@@ -0,0 +1,34 @@
+import json
+from confluent_kafka import Consumer
+from telegram import User
+
+
+from kafka import kafka_operations
+from modules.stt_module.voice_module import audio_to_text
+
+conf = kafka_operations.load_conf('src/kafka/consumer_stt_conf.json')
+
+
+def process_stt_message(message_info):
+    message_info = json.loads(message_info)
+    user = User(**message_info['user'])
+    audio_to_text(message_info['filename'], message_info['ogg_filename'],
+                  message_info['update_id'], user)
+
+
+def main():
+    consumer = Consumer(conf)
+    consumer.subscribe(['stt'])
+
+    while True:
+        message = consumer.poll(0)
+
+        if message is None:
+            continue
+        if message.error():
+            print(f"Consumer error happened: {message.error()}")
+            continue
+
+        print("Connected to Topic: {} and Partition : {}".format(message.topic(), message.partition()))
+        print("Received Message : {} with Offset : {}".format(message.value().decode('utf-8'), message.offset()))
+        process_stt_message(message.value().decode('utf-8'))
---
+++ src/kafka/consumer_tts.py
@@ -0,0 +1,32 @@
+import json
+from confluent_kafka import Consumer
+from telegram import User
+
+
+from kafka import kafka_operations
+from utilities.wrapper import send_voice
+
+conf = kafka_operations.load_conf('src/kafka/consumer_tts_conf.json')
+
+
+def process_tts_message(message_info):
+    message_info = json.loads(message_info)
+    user = User(**message_info['user'])
+    send_voice(message_info['text'], user, message_info['reply_markup'])
+
+
+def main():
+    consumer = Consumer(conf)
+    consumer.subscribe(['tts'])
+    while True:
+        message = consumer.poll(0)
+
+        if message is None:
+            continue
+        if message.error():
+            print(f"Consumer error happened: {message.error()}")
+            continue
+
+        print("Connected to Topic: {} and Partition : {}".format(message.topic(), message.partition()))
+        print("Received Message : {} with Offset : {}".format(message.value().decode('utf-8'), message.offset()))
+        process_tts_message(message.value().decode('utf-8'))
